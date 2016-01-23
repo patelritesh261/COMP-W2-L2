fa97f70dd962acc22abb34e29bea55222d5aa885
patelritesh261 <patelritesh261@gmail.com>
2016-01-22 21:58:09 -0500

    upsated index.js

--- index.ts
+++ index.ts
@@ -2,14 +2,11 @@ import http = require('http');
 
 
 
-const hostname = '127.0.0.1';
-const port:number = 8080;
+
+const port:number = pro.env.PORT || 8080;
 
 http.createServer(function(req:http.ServerRequest,res:http.ServerResponse) {
     res.writeHead(200,{'Content-Type':'text/plain'});
     res.end("Hello Words\n")
-}).listen(port,hostname,function(){
-    
-     console.log("Server running at http://"+hostname+":"+port);
-});
+}).listen(port);
 
