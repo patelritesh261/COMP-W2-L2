d2ab2ecfe4152b761e2c8effe177de67c0b0832e
patelritesh261 <patelritesh261@gmail.com>
2016-01-22 22:24:05 -0500

    update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 var http = require('http');
-var port = pro.env.PORT || 8080;
+var port = process.env.PORT || 8080;
 http.createServer(function (req, res) {
     res.writeHead(200, { 'Content-Type': 'text/plain' });
     res.end("Hello Words\n");
--- index.ts
+++ index.ts
@@ -3,7 +3,7 @@ import http = require('http');
 
 
 
-const port:number = pro.env.PORT || 8080;
+const port:number = process.env.PORT || 8080;
 
 http.createServer(function(req:http.ServerRequest,res:http.ServerResponse) {
     res.writeHead(200,{'Content-Type':'text/plain'});
