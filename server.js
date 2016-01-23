68f8b9825ddac5879b9629cf6b7c5d41aceb334d
patelritesh261 <patelritesh261@gmail.com>
2016-01-22 23:04:23 -0500

    index t0 server

---
+++ server.js
@@ -0,0 +1,7 @@
+var http = require('http');
+var port = process.env.PORT || 8080;
+http.createServer(function (req, res) {
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.end("Hello Words\n");
+}).listen(port);
+//# sourceMappingURL=server.js.map
